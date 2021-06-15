4176692e327d0c6c0f1928b32576170e7f61b873
Santhoosh <santhosh6194@gmail.com>
2021-06-15 21:54:24 +0530

    m307f: Update dalvik heap values for 4G memory

--- device.mk
+++ device.mk
@@ -3,4 +3,7 @@ $(call inherit-product, device/samsung/m30s-common/m30s-common.mk)
 # PROPRIETARY VENDOR FILES #
 $(call inherit-product, vendor/samsung/m307f/m307f-vendor.mk)
 
+# DALVIK #
+$(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
+
 DEVICE_PATH := device/samsung/m307f
