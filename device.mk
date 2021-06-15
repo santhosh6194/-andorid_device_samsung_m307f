202571591414d1e753e3dcbd2f467a18d4fa8169
Santhoosh <santhosh6194@gmail.com>
2021-06-15 22:27:06 +0530

    m307f: define fingerprint sensor location

--- device.mk
+++ device.mk
@@ -7,3 +7,6 @@ $(call inherit-product, vendor/samsung/m307f/m307f-vendor.mk)
 $(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
 
 DEVICE_PATH := device/samsung/m307f
+
+DEVICE_PACKAGE_OVERLAYS += \
+    $(DEVICE_PATH)/overlay-lineage
