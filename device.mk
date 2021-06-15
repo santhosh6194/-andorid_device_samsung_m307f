46b899612b1579cccdade53200dcfcf4216d9e9f
Santhoosh <santhosh6194@gmail.com>
2021-06-15 12:29:16 +0530

    m307f: Initialize device tree files

---
+++ device.mk
@@ -0,0 +1,6 @@
+# COMMON #
+$(call inherit-product, device/samsung/m30s-common/m30s-common.mk)
+# PROPRIETARY VENDOR FILES #
+$(call inherit-product, vendor/samsung/m307f/m307f-vendor.mk)
+
+DEVICE_PATH := device/samsung/m307f
---
+++ lineage_m307f.mk
@@ -0,0 +1,37 @@
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Inherit device configuration #
+$(call inherit-product, device/samsung/m307f/device.mk)
+
+# Boot Animation #
+TARGET_BOOTANIMATION_HALF_RES := true
+TARGET_SCREEN_HEIGHT := 2340
+TARGET_SCREEN_WIDTH := 1080
+
+# Device identifier. This must come after all inclusions #
+PRODUCT_NAME := lineage_m307f
+PRODUCT_DEVICE := m307f
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-M307F
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_PDA_MODEL := M307F
+PRODUCT_PDA_MODEL_VERSION := XXU4CUD2
+PRODUCT_PDA_VERSION := $(PRODUCT_PDA_MODEL)$(PRODUCT_PDA_MODEL_VERSION)
+PRODUCT_SHIPPING_API_LEVEL := 28
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=m30s \
+    PRIVATE_BUILD_DESC="m30s-user 11 RP1A.200720.012 $(PRODUCT_PDA_VERSION) release-keys"
+
+BUILD_FINGERPRINT := samsung/m30dd/m30s:11/RP1A.200720.012/$(PRODUCT_PDA_VERSION):user/release-keys
+
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.build.PDA=$(PRODUCT_PDA_VERSION)
+
+ifneq ($(LINEAGE_DEV_CERTIFICATE),)
+    PRODUCT_DEFAULT_DEV_CERTIFICATE := $(LINEAGE_DEV_CERTIFICATE)
+endif
